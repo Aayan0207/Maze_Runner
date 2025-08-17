d16f5ebeb3a4fa9bb6ec5b0d8804b0d7fb831e90
Aayan Khan <aayan.khan02072007@gmail.com>
2025-08-17 12:08:04 +0530

    Initial

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, session, jsonify
 import secrets
 import os
-import maze_runner
+from . import maze_runner
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", secrets.token_hex(16))
